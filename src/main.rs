2eb18348389280037dd4aaea8ec2ad2909f5c7e2
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-04 00:35:05 +0900

    ファイル内容を変数に格納

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::env;
+use std::{env, fs::File, io::Read};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let filename = &args[2];
-    println!("searching for {} in {}...", query, filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("file contents: \n{}", contents);
 }
