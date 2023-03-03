82f1a853455c323542c7b8fdde68a4c47daf7874
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-04 00:31:00 +0900

    コマンドライン引数を変数に格納

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,7 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let query = &args[1];
+    let filename = &args[2];
+    println!("searching for {} in {}...", query, filename);
 }
