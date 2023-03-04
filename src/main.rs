3db20bfed0aa0a0d4e138a1b5213df37742dc217
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-04 20:40:55 +0900

    ロジックをlib.rsに切り出し

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{env, error::Error, fs::File, io::Read, process};
+extern crate minigrep;
+
+use minigrep::Config;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,37 +10,8 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     };
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    println!("file contents: \n{}", contents);
-
-    Ok(())
 }
