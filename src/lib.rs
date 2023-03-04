be6b6f86d190126acd9e74115cd4fbe8ef2ad904
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-05 00:59:10 +0900

    iteratorを使ってリファクタ

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(a) => a,
+            None => return Err("didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(a) => a,
+            None => return Err("didn't get a file name"),
+        };
 
         Ok(Config { query, filename })
     }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@ use minigrep::Config;
 use std::{env, process};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         println!("[ERR] parsing arguments: {}", err);
         process::exit(1);
     });
