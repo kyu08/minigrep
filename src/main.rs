598577ce8be064d89de8d33b8e324486cad9d2eb
kyu08 <49891479+kyu08@users.noreply.github.com>
2023-03-04 20:32:33 +0900

    エラー処理を追加 処理をrun関数に切り出し

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-use std::{env, fs::File, io::Read};
+use std::{env, fs::File, io::Read, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("[ERR] parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let mut f = File::open(config.filename).expect("file not found");
-
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("file contents: \n{}", contents);
+    run(config);
 }
 
 struct Config {
@@ -20,10 +16,25 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
+}
+
+fn run(config: Config) {
+    let mut f = File::open(config.filename).expect("file not found");
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("file contents: \n{}", contents);
 }
